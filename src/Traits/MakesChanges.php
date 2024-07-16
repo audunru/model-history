63107c3e1af475c9a58b5b715e08fc1389c7d8dc
Audun Rundberg <audun@rundberg.no>
2024-07-16 20:38:37 +0200

    feat: stronger phpdoc typehints (#15)
    * style: types
    
    * feat: stronger phpdoc typehints
    
    * style: add return type

--- src/Traits/MakesChanges.php
+++ src/Traits/MakesChanges.php
@@ -5,10 +5,15 @@ namespace audunru\ModelHistory\Traits;
 use audunru\ModelHistory\Models\Change;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 
+/**
+ * @mixin \Illuminate\Database\Eloquent\Model
+ */
 trait MakesChanges
 {
     /**
      * Get changes made by this owner.
+     *
+     * @return MorphMany<Change, $this>
      */
     public function changes(): MorphMany
     {
